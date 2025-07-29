b802864df7d32a4d5f7dbc2f3e328130700b9069
Emir Berk Öncü <emirberkoncu@gmail.com>
2025-07-29 17:57:13 +0300

    Variables

--- lesson1/index.py
+++ lesson1/index.py
@@ -1,7 +1,19 @@
 
-print('Welcome to Python 101!')
-print('Welcome to Python 1012!')
-print('Create Hammer')
-print('Create Nails')
-print('Use Hammer')
-print('Use Hammer and Nails')
+# print('Welcome to Python 101!')
+# print('Welcome to Python 1012!')
+# print('Create Hammer')
+# print('Create Nails')
+# print('Use Hammer')
+# print('Use Hammer and Nails')
+
+
+
+failed_subjects = "6"
+name = 'John'
+
+print('Dear Mrs Badger')
+print('Your son' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name  + ' will need to redo ' + failed_subjects +  ' courses.')
+name = 'Eric'
+print(name + ' is doing well in geography.')
+
